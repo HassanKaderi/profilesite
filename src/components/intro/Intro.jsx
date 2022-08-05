fe9f9516231c43471abe640a20702c7911a1a8fe
HassanKaderi <HassanImadKaderi@gmail.com>
2022-08-04 19:15:00 -0600

    First Push

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,14 @@
+import"./intro.scss"
+
+export default function Intro() {
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <canvas className="voxelCanvas">
+          
+        </canvas>
+      </div>
+      <div className="right"></div>
+    </div>
+  )
+}
---
+++ src/components/references/References.jsx
@@ -0,0 +1,9 @@
+import "./references.scss"
+
+export default function References() {
+  return (
+    <div className="references" id="references">
+        
+    </div>
+  )
+}
