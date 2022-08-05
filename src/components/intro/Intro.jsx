7c3db702d074b3d9749cad5d95266ed0760b12bc
HassanKaderi <HassanImadKaderi@gmail.com>
2022-08-05 00:57:41 -0600

    more updates

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,8 @@
 import "./intro.scss"
-import { Canvas } from "@react-three/fiber"
+import { Canvas} from "@react-three/fiber"
 import { Suspense } from "react"
+import { OrbitControls } from "@react-three/drei"
+
 
 export default function Intro() {
   return (
@@ -8,12 +10,18 @@ export default function Intro() {
       <div className="left">
         <Canvas>
           <Suspense fallback={null}>
+            <OrbitControls/>
             <ambientLight/>
 
           </Suspense>
         </Canvas>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, i'm</h2>
+          <h1>Hassan Kaderi</h1>
+        </div>
+      </div>
     </div>
   )
 }
