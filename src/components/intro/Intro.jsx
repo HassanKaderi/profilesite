bdfb66e352e8653f20371246981234e7aca717d7
HassanKaderi <HassanImadKaderi@gmail.com>
2022-08-05 18:49:43 -0600

    So Many more updates!!!!

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,22 +1,48 @@
+
 import "./intro.scss"
 import Char from '../model/Model'
 import { Canvas } from "@react-three/fiber"
-import { Suspense } from "react"
+import { Suspense, useEffect, useRef } from "react"
+import { OrbitControls } from "@react-three/drei"
+import { init } from "ityped"
+
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(()=>{
+    //Instead of using doc selector cause im using react
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500, // Miliseconds
+      backSpeed: 60,
+      startDelay: 200,
+      cursorChar: "✏️", // i didnt even know I coulde use emojies this is dope
+      strings: ["Abuuk", "A Developer", "An Artist", "Determined", "Wait this is fun", "Were you expecting more?", "Come on now let me enjoy this"],
+    });
+  },[])
+
   return (
     <div className="intro" id="intro">
       <div className="left">
         <Canvas>
-          <Suspense><Char/></Suspense>
-          
+          <Suspense>
+            <spotLight/>
+            <OrbitControls/>
+            <Char/>
+          </Suspense>
         </Canvas>
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2>Hi there, i'm</h2>
+          <h2>Hi there, I'm</h2>
           <h1>Hassan Kaderi</h1>
+          <h3>... But my friends call me <span ref={textRef}></span></h3>
         </div>
+        <a href="#portfolio">
+          <img src="assets/down-arrow.png" alt=""/>
+        </a>
       </div>
     </div>
   )
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,46 @@
 import "./portfolio.scss"
 
 export default function Portfolio() {
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "template",
+      title: "Template",
+    }
+  ]
+
   return (
     <div className="portfolio" id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="/assets/unpacker-logo.png" alt=""/>
+          <h3>Pokemon Pack Opener</h3>
+        </div>
+        <div className="item">
+          <img src="/assets/unpacker-logo.png" alt=""/>
+          <h3>Pokemon Pack Opener</h3>
+        </div>
+        <div className="item">
+          <img src="/assets/unpacker-logo.png" alt=""/>
+          <h3>Pokemon Pack Opener</h3>
+        </div>
+        <div className="item">
+          <img src="/assets/unpacker-logo.png" alt=""/>
+          <h3>Pokemon Pack Opener</h3>
+        </div>
+      </div>
     </div>
   )
 }
